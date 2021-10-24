8f7369170894319e5d210927b2e363d297f0bade
Deeraj Kumar <deerajkmr98@gmail.com>
2021-10-24 11:22:49 +0530

    customize django admin interface

--- videos/admin.py
+++ videos/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Movie)
-admin.site.register(models.Customer)
+class MovieAdmin(admin.ModelAdmin):
+    fields = ['release_year','title','length']
+
+    search_fields = ['title','length']
+
+    list_filter = ['release_year','title','length']
+
+    list_display = ['title','length','release_year']
+
+    list_editable = ['length']
+
+admin.site.register(models.Customer,)
+admin.site.register(models.Movie,MovieAdmin)
